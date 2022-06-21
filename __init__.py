787924c407938f8d3cdf6aedf050cf39a04b05d0
B3NZ3N3 <84439064+vichubenzene@users.noreply.github.com>
2022-06-21 20:18:17 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# coding:utf-8
-# author VISWANATHAN 
-# version : 0.1
-__all__ = 'google_translator'
-
-from .benzene_translator import google_translator
-from .constant import DEFAULT_SERVICE_URLS, LANGUAGES
