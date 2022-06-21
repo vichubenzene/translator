8b5d2ae4f7ab4b269f0a3dce0c655ab90b3ceacd
B3NZ3N3 <84439064+vichubenzene@users.noreply.github.com>
2022-06-21 20:17:06 +0530

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+# author Viswanathan S
+# version : 0.1
+__all__ = 'google_translator'
+__version__ = '0.2.1'
+
+from .benzene_translator import google_translator
+from .constant import DEFAULT_SERVICE_URLS, LANGUAGES
